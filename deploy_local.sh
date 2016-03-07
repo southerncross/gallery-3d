67a3c71a5c9ed17287a0ac5a0c10c312beb855d9
LiShunyang <lishunyang@pku.edu.cn>
2016-03-07 15:04:11 +0800

    完善部署脚本

--- deploy_local.sh
+++ deploy_local.sh
@@ -5,7 +5,11 @@
 #
 # Note: $GALLERY_3D_USER_HOST should be set in environment already
 # e.g. lishunyang@123.456.789
-#
+
+if [ ! $GALLERY_3D_USER_HOST ]; then
+  echo '\033[31mERROR:\033[0m $GALLERY_3D_USER_HOST not found' && exit 1
+fi
+
 # Step 1: Build and install npm modules
 gulp build && cd dist && npm install --production && cd .. || exit 1
 
@@ -18,3 +22,5 @@ zip -r $zip_filename dist && scp $zip_filename $GALLERY_3D_USER_HOST:~/ && rm -R
 
 # Step 3: Call remote script
 ssh -t $GALLERY_3D_USER_HOST "sh ./deploy_remote.sh ${filename}"
+
+echo "\033[32mDeploy finished!\033[0m"
